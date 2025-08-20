f7115923619502b1695c542e9d70bc882387eb52
Burcin Acar <burcin.acar@crick.ac.uk>
2025-08-20 15:32:14 +0300

    Add files via upload

---
+++ fasta.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Load CSV
+df = pd.read_csv("data/seqs.csv")
+
+# Write to FASTA
+with open("eval/sequences.fasta", "w") as fasta_file:
+    for _, row in df.iterrows():
+        fasta_file.write(f">{row['id']}\n")
+        fasta_file.write(f"{row['original_sequence']}\n")
